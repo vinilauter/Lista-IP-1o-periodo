ae7a371e708ceaad8a61ead3aa6916309d0cd5b3
vinilauter <vinnielauter@gmail.com>
2025-07-21 17:03:23 -0300

    "Início da q3"

---
+++ Lista 6 IP/q3.py
@@ -0,0 +1,19 @@
+﻿# função para a definição dos confrontos
+
+def cruzamento(grupos,grupo_x,grupo_y,chave):
+    print()
+    print(f"Confrontos chave {chave}:")
+    print(f"{grupos[grupo_x][0]} x {grupos[grupo_y][1]}")
+    print(f"{grupos[grupo_x][1]} x {grupos[grupo_y][0]}")
+    chave+=1
+    return chave
+
+# programa principal
+
+qtd_grupos=int(input())
+grupos={}
+chave=1
+
+i=1
+for i in range(qtd_grupos-1):
+    grupos[grupo_i]
