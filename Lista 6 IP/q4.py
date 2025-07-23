fe85adfa4fbea7716235e0fe50ab8029b09a5b1c
vinilauter <vinnielauter@gmail.com>
2025-07-23 18:42:26 -0300

    "q4 finalizada e q5 iniciada"

---
+++ Lista 6 IP/q4.py
@@ -0,0 +1,159 @@
+﻿# função do cálculo da pontuação total
+def calculo_pontuacao(continentais, nacionais, aproveitamento, nacionalidade):
+    pontuacao_total = (continentais*100+nacionais*50)*aproveitamento
+
+    # checagem de nacionalidade
+    if nacionalidade=="brasileiro":
+        pontuacao_total+=pontuacao_total*0.1
+    elif nacionalidade=="alemão":
+        pontuacao_total-=pontuacao_total*0.1
+
+    # checagem de continentais
+    if continentais==0:
+        pontuacao_total-=pontuacao_total*0.5
+
+    return pontuacao_total
+
+# função auxiliar para o ordenamento
+
+def buscar_candidato_pela_pontuacao(pontuacao_alvo, dicionario_candidatos):
+    # Iteramos sobre as chaves (nomes) do dicionário
+    for nome_candidato in dicionario_candidatos:
+        dados=dicionario_candidatos[nome_candidato]
+        if dados['pontuacao']==pontuacao_alvo:
+            return nome_candidato
+        
+# programa principal 
+
+candidatos_dados={}
+candidatos=int(input())
+
+for i in range(candidatos):
+
+    # leitura das informações do candidato
+
+    nome=input()
+
+    if nome=="Ancelotti":
+        print("Será que Carleto irá continuar no cargo?")
+    elif nome=="Jorge Jesus":
+        print("O mister finalmente retornará ao Brasil?")
+
+    nacionalidade=input()
+
+    if nacionalidade=="alemão":
+        print("Iremos mesmo perdoar o 7x1?")
+    if nacionalidade=="argentino":
+        print("Um hermano comandando a seleção? Sai fora!")
+    if nacionalidade!="argentino":
+        continentais=int(input())
+        nacionais=int(input())
+        aproveitamento=float(input())
+        interesse=input()
+
+        # calculo e armazenamento das informações
+
+        pontuacao=calculo_pontuacao(continentais, nacionais, aproveitamento, nacionalidade)
+
+        candidatos_dados[nome]={
+            'nacionalidade': nacionalidade,
+            'pontuacao': pontuacao,
+            'interesse': interesse
+        }
+
+# ordenamento das pontuações 
+
+pontuacoes_extraidas={}
+
+for nome_candidato in candidatos_dados:
+    dados=candidatos_dados[nome_candidato]
+    pontuacoes_extraidas[dados['pontuacao']]=True
+
+pontuacoes_ordenadas={}
+copia_pontuacoes=pontuacoes_extraidas.copy()
+indice=0
+
+while copia_pontuacoes:
+    maior_pontuacao=-1000.0
+    for pontuacao in copia_pontuacoes:
+        if pontuacao>maior_pontuacao:
+            maior_pontuacao=pontuacao
+    
+    pontuacoes_ordenadas[indice]=maior_pontuacao
+    indice=indice+1
+    del copia_pontuacoes[maior_pontuacao]
+
+print("Lista de treinadores - CBF")
+for i in range(3):
+    pontuacao_atual=pontuacoes_ordenadas[i]
+    nome_do_candidato=buscar_candidato_pela_pontuacao(pontuacao_atual, candidatos_dados)
+    
+    dados_do_candidato=candidatos_dados[nome_do_candidato]
+    nacionalidade_candidato=dados_do_candidato['nacionalidade']
+    interesse_candidato=dados_do_candidato['interesse']
+    
+    pontuacao_formatada=f"{pontuacao_atual:.2f}"
+    
+    print(f"{i+1}º {nome_do_candidato} - {nacionalidade_candidato} - {pontuacao_formatada} pontos")
+
+# escolha do técnico
+
+tecnico_escolhido=False
+
+# checagem do ancelotti
+
+ancelotti=False
+
+i=0
+
+while not tecnico_escolhido and i<3:
+    pontuacao_atual=pontuacoes_ordenadas[i]
+    nome_do_candidato=buscar_candidato_pela_pontuacao(pontuacao_atual, candidatos_dados)
+    dados_do_candidato=candidatos_dados[nome_do_candidato]
+    interesse_candidato=dados_do_candidato['interesse']
+    nacionalidade_candidato=dados_do_candidato['nacionalidade']
+    if interesse_candidato=="sim":
+        if nome_do_candidato=="Ancelotti":
+            ancelotti=True
+            print(f"{nome_do_candidato} será o quarto estrangeiro a treinar o Brasil. Que honra para o {nacionalidade_candidato}!")
+            print("Depois de uma longa novela, Carlo Ancelotti continuará como o treinador da Seleção Brasileira! Estamos bem servidos!")
+            tecnico_escolhido=True
+    i=i+1
+
+i=0 
+
+while not tecnico_escolhido and i<3:
+
+    pontuacao_atual=pontuacoes_ordenadas[i]
+    nome_do_candidato=buscar_candidato_pela_pontuacao(pontuacao_atual, candidatos_dados)
+    dados_do_candidato=candidatos_dados[nome_do_candidato]
+    interesse_candidato=dados_do_candidato['interesse']
+    nacionalidade_candidato=dados_do_candidato['nacionalidade']
+
+    # checagem do ancelotti
+    
+    if interesse_candidato=="sim":
+
+        # prints do estrangeiro
+        if nacionalidade_candidato!="brasileiro" and not ancelotti:
+            print(f"{nome_do_candidato} será o quarto estrangeiro a treinar o Brasil. Que honra para o {nacionalidade_candidato}!")
+            tecnico_escolhido=True
+        # casos especiais
+        if nome_do_candidato=="Jorge Jesus":
+            print("JESUS VOLTOU!!! Será que ele conseguirá repetir na seleção o sucesso que obteve no Flamengo?")
+            tecnico_escolhido=True
+        elif nome_do_candidato=="Felipão":
+            print("FELIPÃO DE NOVO!? Vem mais um 7x1 por aí?")
+            tecnico_escolhido=True
+        # caso genérico de técnicos
+        else:
+            print(f"O técnico {nacionalidade_candidato} {nome_do_candidato} irá treinar o Brasil. Não era o nome que esperávamos, mas torcemos para que faça um bom trabalho!")
+            tecnico_escolhido=True
+
+    elif interesse_candidato=="não" and not tecnico_escolhido: 
+        print(f"O {nome_do_candidato} não aceitou a proposta da CBF e outros nomes serão analisados. Cuida, CBF!")
+    
+    i=i+1
+
+if not tecnico_escolhido:
+    print("Nenhum técnico aceitou a maior seleção do mundo!? Que humilhação, Sr. Samir Xaud!!!")
---
+++ Lista 6 IP/q5.py
@@ -0,0 +1,150 @@
+# função para cálculo de pontuação do jogador titular
+
+def calculo_pontuacao(jogador):
+    pontos=0
+
+    # separação dos dados
+
+    nome,posicao,gols_feitos,assistencias,amarelos,vermelhos,gols_sofridos=jogador.split(",")
+
+    # adição de pontos por gol
+
+    for i in range(int(gols_feitos)):
+        pontos+=8
+
+    # adição de pontos por assistências
+
+    for i in range(int(assistencias)):
+        pontos+=4
+
+    # adição de pontos por cartões amarelos
+
+    for i in range(int(amarelos)):
+        pontos-=1
+
+    # adição de pontos por cartões vermelhos
+
+    for i in range(int(vermelhos)):
+        pontos-=3
+
+    # adição de pontos por clean sheet
+
+    if posicao=="goleiro" or posicao=="lateral" or posicao=="zagueiro":
+        if gols_sofridos=="0":
+            pontos+=5
+        if gol_sofrido!="0":
+            gol_sofrido=True
+    return pontos,gol_sofrido,posicao,nome
+
+# programa principal
+
+# recepção dos técnicos 
+
+n_tecnicos=int(input())
+
+for i in range(n_tecnicos):
+    # input do técnico
+
+    nome_tecnico=input()
+    escalacao=input()
+
+    # listas e dicionários para cada posição
+
+    pontos_goleiros=[]
+    pontos_zagueiros=[]
+    pontos_laterais=[]
+    pontos_meias=[]
+    pontos_atacantes=[]
+
+    goleiros_lista=[]
+    zagueiros_lista=[]
+    laterais_lista=[]
+    meias_lista=[]
+    atacantes_lista=[]
+
+    # pontos totais do time
+
+    pontos_time=0
+
+    # condicionais para recepção de titulares ou reservas
+     
+    if escalacao=="titulares":
+
+        for i in range(11):
+            jogador=input()
+            gol_sofrido=False
+            pontos,gol_sofrido,posicao,nome=calculo_pontuacao(jogador,gol_sofrido)
+            if posicao=="goleiro":
+                pontos_goleiros.append(pontos)
+                goleiros_lista.append(nome)
+                pontos_time+=pontos
+            elif posicao=="zagueiro":
+                pontos_zagueiros.append(pontos)
+                pontos_time+=pontos
+            elif posicao=="lateral":
+                pontos_laterais.append(pontos)
+                laterais_lista.append(nome)
+                pontos_time+=pontos
+            elif posicao=="meia":
+                pontos_meias.append(pontos)
+                meias_lista.append(nome)
+                pontos_time+=pontos 
+            elif posicao=="atacante":
+                pontos_atacantes.append(pontos)
+                atacantes_lista.append(nome)
+                pontos_time+=pontos
+
+        # agora os reservas
+
+        escalacao=input()
+        for i in range(5):
+            jogador=input()
+            pontos,gol_sofrido,posicao=calculo_pontuacao(jogador,gol_sofrido)
+            # pontuações hipotéticas se houver substituição
+            if posicao=="goleiro":
+                indice_minimo_goleiros=pontos_goleiros.index(min(pontos_goleiros))
+                if min(pontos_goleiros)<pontos:
+                    pontos_time_copia_goleiro=pontos_time
+                    pontos_time_copia_goleiro=pontos_time_copia_goleiro-pontos_goleiros[indice_minimo_goleiros]+pontos
+                    nome_goleiro=nome
+            elif posicao=="zagueiro":
+                indice_minimo_zagueiros=pontos_zagueiros.index(min(pontos_zagueiros))
+                if min(pontos_zagueiros)<pontos:
+                    pontos_time_copia_zagueiro=pontos_time
+                    pontos_time_copia_zagueiro=pontos_time_copia_zagueiro-pontos_zagueiros[indice_minimo_zagueiros]+pontos
+                    nome_zagueiro=nome
+            elif posicao=="lateral":
+                indice_minimo_laterais=pontos_laterais.index(min(pontos_laterais))
+                if min(pontos_laterais)<pontos:
+                    pontos_time_copia_lateral=pontos_time
+                    pontos_time_copia_lateral=pontos_time_copia_lateral-pontos_laterais[indice_minimo_laterais]+pontos
+                    nome_lateral=nome
+            elif posicao=="meia":
+                indice_minimo_meias=pontos_meias.index(min(pontos_meias))
+                if min(pontos_meias)<pontos:
+                    pontos_time_copia_meia=pontos_time
+                    pontos_time_copia_meia=pontos_time_copia_meia-pontos_meias[indice_minimo_meias]+pontos
+                    nome_meia=nome
+            elif posicao=="atacante":
+                indice_minimo_atacantes=pontos_atacantes.index(min(pontos_atacantes))
+                if min(pontos_atacantes)<pontos:
+                    pontos_time_copia_atacante=pontos_time
+                    pontos_time_copia_atacante=pontos_time_copia_atacante-pontos_atacantes[indice_minimo_atacantes]+pontos
+                    nome_atacante=nome
+            # pontuação máxima com substituição
+            maximo_pontos=max(pontos_time_copia_goleiro,pontos_time_copia_zagueiro,pontos_time_copia_lateral,pontos_time_copia_meia,pontos_time_copia_atacante)
+        if maximo_pontos!=pontos_time:
+            pontos_time=maximo_pontos
+            if pontos_time_copia_goleiro==maximo_pontos:
+                print(f"{nome_tecnico} é um gênio da bola mesmo, a substituição de {goleiros_lista[indice_minimo_goleiros]} por {nome_goleiro} fez ele ganhar pontos!")
+            elif pontos_time_copia_zagueiro==maximo_pontos:
+                print(f"{nome_tecnico} é um gênio da bola mesmo, a substituição de {zagueiros_lista[indice_minimo_zagueiros]} por {nome_zagueiro} fez ele ganhar pontos!")
+            elif pontos_time_copia_lateral==maximo_pontos:
+                print(f"{nome_tecnico} é um gênio da bola mesmo, a substituição de {laterais_lista[indice_minimo_laterais]} por {nome_lateral} fez ele ganhar pontos!")
+            elif pontos_time_copia_meia==maximo_pontos:
+                print(f"{nome_tecnico} é um gênio da bola mesmo, a substituição de {meias_lista[indice_minimo_meias]} por {nome_meia} fez ele ganhar pontos!")
+            elif pontos_time_copia_atacante==maximo_pontos:
+                print(f"{nome_tecnico} é um gênio da bola mesmo, a substituição de {atacantes_lista[indice_minimo_atacantes]} por {nome_atacante} fez ele ganhar pontos!")
+            
+
+                        
